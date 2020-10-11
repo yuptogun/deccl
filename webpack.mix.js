d914d47c651f21755486279b47476f5cd6ac0436
Eojin Kim <eojin1211@hanmail.net>
2020-10-11 15:57:55 +0900

    헬퍼파일 추가, mix() 함수 추가

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 let mix = require('laravel-mix');
 
 mix .js('resources/js/deccl.js', 'public/js/')
-    .sass('resources/css/deccl.scss', 'public/css/');
+    .sass('resources/css/deccl.scss', 'public/css/')
+    .version()
+    .disableNotifications();
