594ceb023af84671ca3c96ad7cfc812370b49d36
Eojin Kim <eojin1211@hanmail.net>
2020-10-18 22:37:09 +0900

    기본 DB 마이그레이션 추가

---
+++ database/migrations/2020_10_18_210002_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('url')->comment('기사 URL');
+            $table->string('title')->comment('기사 제목');
+            $table->string('summary')->nullable()->comment('기사 요약');
+            $table->string('thumbnail')->nullable()->comment('기사 사진');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
