b75b9d7a30f4594a013baf94dceb0e31c910facf
Eojin Kim <eojin1211@hanmail.net>
2021-02-05 23:12:47 +0900

    Free Sans 적용 Fixes #36

--- resources/views/_.blade.php
+++ resources/views/_.blade.php
@@ -31,7 +31,8 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="{{ url('images/favicon/ms-icon-144x144.png') }}">
     <meta name="theme-color" content="#ffffff">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;700&family=Noto+Sans+KR:wght@300;700&display=swap" rel="stylesheet">
+    {{-- <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;700&family=Noto+Sans+KR:wght@300;700&display=swap" rel="stylesheet"> --}}
+    <link href="{{ url('fonts/free-sans/FreeSans.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ mix('css/deccl.css') }}">
     @stack('styles')
     @stack('scripts')
