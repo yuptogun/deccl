92408a0611d6fffe9f39162bf20951ce0d4a3ce9
Eojin Kim <eojin1211@hanmail.net>
2021-02-13 16:45:24 +0900

    comment.create 에 URL 넘겨서 바로 작성 시작되도록 Fixes #44

--- resources/views/comment/create.blade.php
+++ resources/views/comment/create.blade.php
@@ -3,14 +3,15 @@
 @push('body_after')
 <script>
 (function ($) {
-    let findOrNew = "{{ route('api.article.findOrNew') }}";
     let articleRadioContainer = $('#article_radio');
     let articleSearchInput = $('#article_url');
     let commentInput = $('#comment');
+    let findOrNewArticle = "{{ route('api.article.findOrNew') }}";
+    let articleUrlRequested = "{{ request()->input('url') }}";
 
-    articleSearchInput.on('change', function (e) {
-        let searchValue = $(e.target).val();
-        let ajaxConfig = getAJAXConfig(findOrNew, 'POST', {search: searchValue});
+    let getArticleOptions = function () {
+        let searchValue = articleSearchInput.val(); // 의도적으로 길이 체크를 하지 않음.
+        let ajaxConfig = getAJAXConfig(findOrNewArticle, 'POST', {search: searchValue});
         articleRadioContainer.slideUp().html('');
         $.ajax(ajaxConfig)
         .done(function (data) {
@@ -21,8 +22,14 @@
                 articleRadioContainer.slideDown();
             }
         });
-    });
+    };
 
+    articleSearchInput.on('change', function () {
+        getArticleOptions();
+    });
+    if (articleUrlRequested) {
+        getArticleOptions();
+    }
     commentInput.summernote({
         height: 240,
         placeholder: '이 기사에 대해 한마디 해 주세요.',
@@ -46,7 +53,7 @@
                 <div class="form-group row">
                     <label class="col-md-3 col-lg-2 col-form-label">기사</label>
                     <div class="col-md-9 col-lg-10">
-                        <input type="url" id="article_url" name="url" class="form-control" placeholder="기사 제목 또는 URL 입력하여 검색" />
+                        <input type="url" id="article_url" name="url" class="form-control" placeholder="기사 제목 또는 URL로 검색" value="{!! request()->input('url') !!}" />
                     </div>
                 </div>
                 <div class="form-group row">
