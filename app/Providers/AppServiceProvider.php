670b23daa66cc9e81fee21d4ae1c77377f9ed1e2
Eojin Kim <eojin1211@hanmail.net>
2021-02-14 13:58:14 +0900

    상용서비스 개시 준비 Fixes #16

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,11 +1,17 @@
 <?php
-
 namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
 
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+    }
+
     /**
      * Register any application services.
      *
