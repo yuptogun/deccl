a2844e799aa0804a76b80c3698b00c1bf104ece2
07000176 <eojin@wjcompass.com>
2021-02-15 09:46:21 +0900

    Summary 너무 길면 INSERT 실패함 Fixes #59

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,7 +9,7 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        Schema::defaultStringLength(env('DB_DEFAULT_LENGTH', 191));
     }
 
     /**
